5f2dda296dd3894148e8cb86ced4850398c0e20a
Akbar <aumetov7@gmail.com>
2019-11-23 09:44:13 +0300

    My third task in second course

--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,69 @@
 package com.efimchick.ifmo.web.jdbc;
 
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+        return resultSet -> {
+            try {
+                Set<Employee> employeeSet = new LinkedHashSet<>();
+
+                while (resultSet.next()) {
+                    employeeSet.add(getEmployee(resultSet));
+                }
+                return employeeSet;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        };
+    }
+
+    private Employee getEmployee(ResultSet resultSet) throws SQLException {
+        try {
+            Employee manager = null;
+
+            Integer managerId = Integer.valueOf(resultSet.getString("MANAGER"));
+            Integer rowNumber = Integer.valueOf(String.valueOf(resultSet.getRow()));
+
+            if (managerId == null) {
+                return null;
+            }
+
+            resultSet.beforeFirst();
+
+            while (resultSet.next()) {
+                if (managerId.equals(Integer.valueOf(resultSet.getString("ID")))) {
+                    manager = getEmployee(resultSet);
+                    break;
+                }
+            }
+            resultSet.absolute(rowNumber);
+
+            return new Employee(
+                    new BigInteger(resultSet.getString("ID")),
+                    new FullName(
+                            resultSet.getString("FIRSTNAME"),
+                            resultSet.getString("LASTNAME"),
+                            resultSet.getString("MIDDLENAME")),
+                    Position.valueOf(resultSet.getString("POSITION")),
+                    LocalDate.parse(resultSet.getString("HIREDATE")),
+                    resultSet.getBigDecimal("SALARY"),
+                    manager
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
